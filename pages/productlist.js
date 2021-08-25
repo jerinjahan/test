ebd2243a6ea29c3064db473d9cdcb9e158c6a20f
jerinjahan <jerinjahan@ymail.com>
2021-08-25 12:45:11 +0600

    image url fixed

--- pages/productlist.js
+++ pages/productlist.js
@@ -9,47 +9,48 @@ export async function getStaticProps() {
     const data = await res.json()
     console.log(data[0].price)
     return {
-      props: {
-        data
-      }
+        props: {
+            data
+        }
     }
-  }
-  /*module.exports = {
-    images: {
-      domains: ['assets.example.com'],
-    },
-  }
-  */
-export default function Productlist({data}) {
-  //  console.log(data)
-    
- return (
-  <>
-  <div>
- 
-    <Title title="Product list" />
-    <Category/>
-    
-        <div className="card">
-           
-       </div>
-   
-      {data.map((data,i) => (
-       <div key={i}>
-         <img src={data.image} width={200} height={200} />
-          <h1 >{data.title}</h1>
-          <h3 className="price">${data.price}</h3>
-          <h4>{data.category}</h4>
-          <p>{data.description}</p>
-          <p><button>Add to Cart</button></p>
-        </div>
-      
-      ))}
- 
-     
-   
-    
-   {/*  <div className="card">
+}
+/*module.exports = {
+  images: {
+    domains: ['assets.example.com'],
+  },
+}
+*/
+export default function Productlist({ data }) {
+    //  console.log(data)
+
+    return (
+        <>
+            <div>
+
+                <Title title="Product list" />
+                <Category />
+
+                <div className="card">
+
+                </div>
+
+                {data.map((data, i) => (
+                    <div key={i}>
+                        {/* <img src={data.image} width={200} height={200} /> */}
+                        <Image src={data?.image} width={200} height={200}></Image>
+                        <h1 >{data.title}</h1>
+                        <h3 className="price">${data.price}</h3>
+                        <h4>{data.category}</h4>
+                        <p>{data.description}</p>
+                        <p><button>Add to Cart</button></p>
+                    </div>
+
+                ))}
+
+
+
+
+                {/*  <div className="card">
         <Image src="/https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width={200} height={200} />
         <h1>Pant</h1>
         <p class="price">$19.99</p>
@@ -71,9 +72,9 @@ export default function Productlist({data}) {
         <p><button>Add to Cart</button></p>
     </div>
     */}
- 
-     
-   </div>
-  </>
- )
+
+
+            </div>
+        </>
+    )
 }
